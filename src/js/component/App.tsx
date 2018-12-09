2b5074fb692adda5e09d973454b4e53a69c90c70
Thanh Huynh <hu.thanh89@yahoo.com>
2018-12-08 21:21:19 -0800

    More ts conversion

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,18 +2,24 @@
 // Import
 //-----------------------------------------------------------------------------//
 
-import React       from 'react';
 import Header      from './Header/Layout';
 import Instruction from './instruction/Layout';
 import Inventory   from '../container/inventory';
 import Workspace   from '../container/workspace';
 import { Route }   from "react-router-dom";
+import * as React  from 'react';
+
+//-----------------------------------------------------------------------------//
+
+type Props = {
+  history: Array<string>
+}
 
 //-----------------------------------------------------------------------------//
 // Component
 //-----------------------------------------------------------------------------//
 
-class App extends React.Component {
+class App extends React.Component<Props> {
   render(){
     return (
       <div>
